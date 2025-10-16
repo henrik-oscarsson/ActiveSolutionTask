44e1cd62a3ee6116ccd1eaa7d2ad4d0268927b52
Henrik Oscarsson <henrik.oscarsson@quickbit.com>
2025-10-16 16:19:05 +0200

    Added tests

--- AspireApp1/AspireApp1.ApiService.Tests/BookingServiceTests.cs
+++ AspireApp1/AspireApp1.ApiService.Tests/BookingServiceTests.cs
@@ -18,14 +18,40 @@ public class BookingServiceTests
     {
         return new BookingService(_mockBookingDbContext.Object, _mockVehicleDbContext.Object, _mockSettingsDbContext.Object, _mockLogger.Object);
     }
+    
     [Fact]
-    public async Task GetAllBookings_CallsDbContext()
+    public async Task GetAllBookings_CallsBookingContext_AndNotVehicleContext_WhenListIsEmpty()
     {
         var bookingService = CreateService();
-        await bookingService.GetAllBookings();
+        _mockBookingDbContext.Setup(b => b.GetAllBookings()).ReturnsAsync(new List<BookingInfo>());
+        var actual = await bookingService.GetAllBookings();
         _mockBookingDbContext.Verify(x => x.GetAllBookings(), Times.Once);
+        _mockVehicleDbContext.Verify(x => x.GetAllVehicles(), Times.Once);
+        _mockSettingsDbContext.Verify(x => x.GetSettings(), Times.Once);
+        Assert.Empty(actual);
     }
-    
+
+    [Fact]
+    public async Task GetAllBookings_CalculatesEstimatedCost()
+    {
+        // Arrange
+        var bookingService = CreateService();
+        var vehicleId = 5;
+        var bookingId = 7;
+        _mockBookingDbContext.Setup(x => x.GetAllBookings()).ReturnsAsync(new List<BookingInfo> { new BookingInfo { Id = bookingId, VehicleId = vehicleId, EstimatedCost = 100 } });
+        _mockVehicleDbContext.Setup(x => x.GetAllVehicles()).ReturnsAsync(new List<Vehicle> {new Vehicle { Id = vehicleId, Category = VehicleCategory.Sedan, RegistrationNumber = "ABC13", MeterSetting = 500 }});
+        _mockSettingsDbContext.Setup(x => x.GetSettings()).ReturnsAsync(Setting.CreateDefault());        
+        // Act
+        var actual  =await bookingService.GetAllBookings();
+        // Assert
+        _mockBookingDbContext.Verify(x => x.GetAllBookings(), Times.Once);
+        _mockVehicleDbContext.Verify(x => x.GetAllVehicles(), Times.Once);
+        _mockSettingsDbContext.Verify(x => x.GetSettings(), Times.Once);
+        var booking = actual.First();
+        Assert.Equal(booking.VehicleId, vehicleId);
+        Assert.Equal(300m, booking.EstimatedCost);
+    }
+
     [Fact]
     public async Task GetAllAvailableBookings_Throws_IfReturnDateIsBeforeStartDate()
     {
@@ -113,5 +139,40 @@ public class BookingServiceTests
         var cost = bookingService.CalculateCost(VehicleCategory.Truck, 2m, 5, 10m, 4);
         // Assert
         Assert.Equal(55m, cost);
+    }
+    
+    [Fact]
+    public void Pickup_UpdatesBooking_And_Vehicle()
+    {
+        // Arrange
+        var bookingService = CreateService();
+        var vehicleId = 5;
+        var bookingId = 7;
+        var meterSetting = 500; 
+        _mockBookingDbContext.Setup(x => x.GetBooking(It.IsAny<int>())).ReturnsAsync(new Booking { Id = bookingId, VehicleId = vehicleId });
+        _mockVehicleDbContext.Setup(x => x.GetVehicle(It.IsAny<int>())).ReturnsAsync(new Vehicle { Id = vehicleId, Category = VehicleCategory.Sedan, RegistrationNumber = "ABC13", MeterSetting = meterSetting });
+        // Act
+        var cost = bookingService.Pickup(42);
+        // Assert
+        _mockBookingDbContext.Verify(x => x.GetBooking(It.IsAny<int>()), Times.Once);
+        _mockVehicleDbContext.Verify(x => x.GetVehicle(It.IsAny<int>()), Times.Once);
+        _mockBookingDbContext.Verify(x => x.Pickup(It.IsAny<int>(), meterSetting), Times.Once);
+    }
+    
+    [Fact]
+    public void Return_UpdatesBooking_And_Vehicle()
+    {
+        // Arrange
+        var bookingService = CreateService();
+        var vehicleId = 5;
+        var bookingId = 7;
+        var meterSetting = 500; 
+        _mockBookingDbContext.Setup(x => x.GetBooking(It.IsAny<int>())).ReturnsAsync(new Booking { Id = bookingId, VehicleId = vehicleId });
+        // Act
+        var cost = bookingService.Return(42, meterSetting);
+        // Assert
+        _mockBookingDbContext.Verify(x => x.GetBooking(It.IsAny<int>()), Times.Once);
+        _mockBookingDbContext.Verify(x => x.Return(It.IsAny<int>(), meterSetting), Times.Once);
+        _mockVehicleDbContext.Verify(x => x.Return(It.IsAny<int>(), meterSetting), Times.Once);
     }
 }
